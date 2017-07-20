ae72e59549e383a674010179afdaa0f39c956f5c
hadoop <hadoop@localhost>
2017-07-20 02:06:13 -0700

    ISS002 Chapter06, Chapter07 example

---
+++ hadoop-fcis-jaegeun/src/main/java/chapter07/CarrierCodeMapper.java
@@ -0,0 +1,25 @@
+package chapter07;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import chapter07.CarrierCodeParser;
+
+public class CarrierCodeMapper extends Mapper<LongWritable, Text, TaggedKey, Text> {
+  TaggedKey outputKey = new TaggedKey();
+  Text outValue = new Text();
+
+  public void map(LongWritable key, Text value, Context context)
+    throws IOException, InterruptedException {
+
+    CarrierCodeParser parser = new CarrierCodeParser(value);
+
+    outputKey.setCarrierCode(parser.getCarrierCode());
+    outputKey.setTag(0);
+    outValue.set(parser.getCarrierName());
+
+    context.write(outputKey, outValue);
+  }
+}
